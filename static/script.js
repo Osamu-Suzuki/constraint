b581a31cb7ae90cbaf5668b76d6b77e0cda587bf
Osamu-Suzuki <69491893+Osamu-Suzuki@users.noreply.github.com>
2023-06-24 21:03:07 +0900

    ４節リンクが実装できた

--- static/script.js
+++ static/script.js
@@ -1,47 +1,48 @@
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
 let data = {
-    a: {x: 200, y: 100},
-    b: {x: 200, y: 300},
+    a: {x: 300, y: 300},
+    b: {x: 300, y: 500},
     c: {x: 500, y: 400},
-    d: {x: 500, y: 100},
-    displacement: [100, -20]
-}
+    d: {x: 500, y: 300},
+    displacement: [0, 0]
+};
 
-fetch('/optimize', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(r_data => {
-    // 最適化後の新しい座標を取得
-    let newCoordinates = r_data;
+canvas.addEventListener('mousemove', (event) => {
+    // マウスカーソルの位置を取得
+    let mouseX = event.clientX - canvas.offsetLeft;
+    let mouseY = event.clientY - canvas.offsetTop;
 
-    // 新しい座標をcanvasに描画
-    let canvas = document.getElementById('myCanvas');
-    let ctx = canvas.getContext('2d');
+    // c点とマウスカーソルの位置との相対位置を計算
+    data.displacement[0] = mouseX - data.c.x;
+    data.displacement[1] = mouseY - data.c.y;
 
-    console.log(data);
+    // /optimize エンドポイントにデータを送信
+    fetch('/optimize', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(r_data => {
+        // 最適化後の新しい座標を取得
+        let newCoordinates = r_data;
 
-    // 初期座標描画
-    ctx.beginPath();
-    ctx.moveTo(data.a.x, data.a.y);
-    ctx.lineTo(data.b.x, data.b.y);
-    ctx.lineTo(data.c.x, data.c.y);
-    ctx.lineTo(data.d.x, data.d.y);
-    ctx.lineTo(data.a.x, data.a.y);
-    ctx.stroke();
+        // canvasをクリア
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // 最終座標描画
-    ctx.beginPath();
-    ctx.moveTo(data.a.x, data.a.y);
-    ctx.lineTo(newCoordinates.b.x, newCoordinates.b.y);
-    ctx.lineTo(newCoordinates.c.x, newCoordinates.c.y);
-    ctx.lineTo(data.d.x, data.d.y);
-    ctx.lineTo(data.a.x, data.a.y);
-    ctx.stroke();
-})
-.catch((error) => {
-    console.error('Error:', error);
+        // 新しい座標をcanvasに描画
+        ctx.beginPath();
+        ctx.moveTo(data.a.x, data.a.y);
+        ctx.lineTo(newCoordinates.b.x, newCoordinates.b.y);
+        ctx.lineTo(newCoordinates.c.x, newCoordinates.c.y);
+        ctx.lineTo(data.d.x, data.d.y);
+        ctx.lineTo(data.a.x, data.a.y);
+        ctx.stroke();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 });
